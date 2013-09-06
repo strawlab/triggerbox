0cb970a6d8fac13fed4a31fb2b95f9329007c252
Andrew Straw <strawman@astraw.com>
2013-09-06 11:00:20 +0200

    include IORodeo's MCP4822 library

--- firmware/triggerbox/triggerbox.ino
+++ firmware/triggerbox/triggerbox.ino
@@ -24,6 +24,331 @@ clock.
 
 #include <Arduino.h>
 #include <EEPROM.h>
+#include <SPI.h>
+
+#define WITH_AOUT
+#ifdef WITH_AOUT
+
+// ----------------------- start MCP4822 library
+
+/* This MCP4822 code is included in this file so the whole firmware
+   can be distributed as a single Arduino file without having to
+   install libraries. It was originally written by Will Dickson, IO
+   Rodeo Inc. It was downloaded from
+   https://bitbucket.org/iorodeo/iorodeo_arduino_libs . It is licensed
+   under the Apache 2.0 licence.
+
+*/
+
+#define MCP4822_NUMCHAN 2
+
+enum MCP4822_DAC_CHAN {MCP4822_DAC_A, MCP4822_DAC_B};
+
+class MCP4822 {
+private:
+    int cs;
+    int ldac;
+    int gain[MCP4822_NUMCHAN];
+    int getCmdWrd(int dac, int value);
+public:
+    MCP4822();
+    MCP4822(int csPin, int ldacPin);
+    void setValue(int dac, int value);
+    void setValue_A(int value);
+    void setValue_B(int value);
+    void setValue_AB(int value_A, int value_B);
+    void setGain2X(int dac);
+    void setGain2X_A();
+    void setGain2X_B();
+    void setGain2X_AB();
+    void setGain1X(int dac);
+    void setGain1X_A();
+    void setGain1X_B();
+    void setGain1X_AB();
+    void off_AB();
+    void off_A();
+    void off_B();
+    void off(int dac);
+};
+// ----------------------------------------------------------------------------
+// max4822.cpp
+//
+// Provides an SPI based interface to the MCP4822 dual voltage output digital
+// to analog converter.
+//
+// Author: Will Dickson, IO Rodeo Inc.
+// ----------------------------------------------------------------------------
+
+#define ACTIVE      0b0001000000000000
+#define SHUTDOWN    0b0000000000000000
+#define GAIN_2X     0b0000000000000000
+#define GAIN_1X     0b0010000000000000
+#define DAC_A_WRITE 0b0000000000000000
+#define DAC_B_WRITE 0b1000000000000000
+
+MCP4822::MCP4822() {
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::MCP4822
+//
+// Constructor
+// ----------------------------------------------------------------------------
+MCP4822::MCP4822(int csPin, int ldacPin) {
+    // Configure chip select and latch pins
+    cs = csPin;
+    ldac = ldacPin;
+    pinMode(cs,OUTPUT);
+    pinMode(ldac,OUTPUT);
+    digitalWrite(cs,HIGH);
+    digitalWrite(ldac,HIGH);
+    // Set to default configuration
+    setGain2X_AB();
+}
+
+// ---------------------------------------------------------------------------
+// MCP4822::getCmdWrd
+//
+// Gets command work for writing value to given channel
+// ---------------------------------------------------------------------------
+int MCP4822::getCmdWrd(int dac, int value) {
+    int cmdWrd = 0;
+    switch (dac) {
+        case MCP4822_DAC_A:
+            cmdWrd = DAC_A_WRITE;
+            break;
+        case MCP4822_DAC_B:
+            cmdWrd = DAC_B_WRITE;
+            break;
+        default:
+            return 0;
+    }
+    cmdWrd |= gain[dac];
+    cmdWrd |= ACTIVE;
+    cmdWrd |= (0b0000111111111111 & value);
+    return cmdWrd;
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::setValue
+//
+// Set the output value of the given dac channel
+// ----------------------------------------------------------------------------
+void MCP4822::setValue(int dac, int value) {
+    int cmdWrd;
+    uint8_t byte0;
+    uint8_t byte1;
+
+    // Enable SPI communications
+    digitalWrite(cs,LOW);
+    // Send command word
+    cmdWrd = getCmdWrd(dac,value);
+    byte0 = cmdWrd >> 8;
+    byte1 = cmdWrd & 0b0000000011111111;
+    SPI.transfer(byte0);
+    SPI.transfer(byte1);
+    // Disable SPI communications
+    digitalWrite(cs,HIGH);
+    // Latch value
+    digitalWrite(ldac,LOW);
+    digitalWrite(ldac,HIGH);
+}
+
+// ---------------------------------------------------------------------------
+// MCP4822::setValue_A
+//
+// Set the output value of dac A to the given value
+// ---------------------------------------------------------------------------
+void MCP4822::setValue_A(int value) {
+    setValue(MCP4822_DAC_A, value);
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::setValue_B
+//
+// Set the output value of dac B to the given value
+// ----------------------------------------------------------------------------
+void MCP4822::setValue_B(int value) {
+    setValue(MCP4822_DAC_B,value);
+}
+
+// ---------------------------------------------------------------------------
+// MCP4822::setValue_AB
+//
+// Set the output value of dac A and B to the given values. Latch them in at
+// the same time.
+// ---------------------------------------------------------------------------
+void MCP4822::setValue_AB(int value_A, int value_B) {
+    int cmdWrd;
+    uint8_t byte0;
+    uint8_t byte1;
+    // Enable SPI communications and send command word
+    digitalWrite(cs,LOW);
+    // Send command word for DAC A
+    cmdWrd = getCmdWrd(MCP4822_DAC_A,value_A);
+    byte0 = cmdWrd >> 8;
+    byte1 = cmdWrd & 0b0000000011111111;
+    SPI.transfer(byte0);
+    SPI.transfer(byte1);
+    // Toggle CS
+    digitalWrite(cs,HIGH);
+    digitalWrite(cs,LOW);
+    // Send command word for DAC A
+    cmdWrd = getCmdWrd(MCP4822_DAC_B,value_B);
+    byte0 = cmdWrd >> 8;
+    byte1 = cmdWrd & 0b0000000011111111;
+    SPI.transfer(byte0);
+    SPI.transfer(byte1);
+    // Disable SPI communications
+    digitalWrite(cs,HIGH);
+    // Latch value
+    digitalWrite(ldac,LOW);
+    digitalWrite(ldac,HIGH);
+
+}
+
+// ---------------------------------------------------------------------------
+// MCP4822::setGain2X
+//
+// Set the gain of the given channel to 2X
+// ---------------------------------------------------------------------------
+void MCP4822::setGain2X(int dac) {
+    if ((dac == MCP4822_DAC_A) || (dac == MCP4822_DAC_B)) {
+        gain[dac] = GAIN_2X;
+    }
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::setGain2X_A
+//
+// Set the gain of dac A to 2X
+// ----------------------------------------------------------------------------
+void MCP4822::setGain2X_A() {
+    setGain2X(MCP4822_DAC_A);
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::setGain2X_B
+//
+// Set the gain of dac B to 2X
+// ----------------------------------------------------------------------------
+void MCP4822::setGain2X_B() {
+    setGain2X(MCP4822_DAC_B);
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::setGain2X_AB
+//
+// Set the gain of dac A and B to 2X
+// ----------------------------------------------------------------------------
+void MCP4822::setGain2X_AB() {
+    setGain2X_A();
+    setGain2X_B();
+}
+
+// ---------------------------------------------------------------------------
+// MCP4822::setGain1X
+//
+// Set the gain of the given channel to 1X
+// ----------------------------------------------------------------------------
+void MCP4822::setGain1X(int dac) {
+    if ((dac == MCP4822_DAC_A) || (dac == MCP4822_DAC_B)) {
+        gain[dac] = GAIN_1X;
+    }
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::setGain1X_A
+//
+// Set the gain of dac A to 1X
+// ----------------------------------------------------------------------------
+void MCP4822::setGain1X_A() {
+    setGain1X(MCP4822_DAC_A);
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::setGain1X_B
+//
+// Set the gain of dac B to 1X
+// ----------------------------------------------------------------------------
+void MCP4822::setGain1X_B() {
+    setGain1X(MCP4822_DAC_B);
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::setGain1X_AB
+//
+// Set the gain of dac A and B to 1X
+// ----------------------------------------------------------------------------
+void MCP4822::setGain1X_AB() {
+    setGain1X_A();
+    setGain1X_B();
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::off
+//
+// Turn off the given dac
+// ----------------------------------------------------------------------------
+void MCP4822::off(int dac) {
+    int cmdWrd = 0;
+    uint8_t byte0;
+    uint8_t byte1;
+
+    // Create shutdown cmd word.
+    switch (dac) {
+        case MCP4822_DAC_A:
+            cmdWrd = DAC_A_WRITE;
+            break;
+        case MCP4822_DAC_B:
+            cmdWrd = DAC_B_WRITE;
+            break;
+        default:
+            return;
+    }
+    cmdWrd |= SHUTDOWN;
+    // Enable SPI communications
+    digitalWrite(cs,LOW);
+    // Send command word
+    byte0 = cmdWrd >> 8;
+    byte1 = cmdWrd & 0b0000000011111111;
+    SPI.transfer(byte0);
+    SPI.transfer(byte1);
+    // Disable SPI communications
+    digitalWrite(cs,HIGH);
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::off_A
+//
+// Turn off dac A
+// ----------------------------------------------------------------------------
+void MCP4822::off_A() {
+    off(MCP4822_DAC_A);
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::off_B
+//
+// Turn off dac B
+// ----------------------------------------------------------------------------
+void MCP4822::off_B() {
+    off(MCP4822_DAC_B);
+}
+
+// ----------------------------------------------------------------------------
+// MCP4822::off_AB
+//
+// Turn off dac A and B
+// ----------------------------------------------------------------------------
+void MCP4822::off_AB() {
+    off_A();
+    off_B();
+}
+
+#endif // WITH_AOUT
+
+// ----------------------- end MCP4822 library
 
 typedef uint32_t pulsenumber_dtype; /* 2**32 @100Hz = 497 days */
 
