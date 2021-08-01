5f51743e590a89d920aaed669682f9609355205e
Andrew Straw <strawman@astraw.com>
2021-08-01 22:03:00 +0200

    reformat triggerbox.ino

--- firmware/triggerbox/triggerbox.ino
+++ firmware/triggerbox/triggerbox.ino
@@ -43,14 +43,20 @@ clock.
 
 #define MCP4822_NUMCHAN 2
 
-enum MCP4822_DAC_CHAN {MCP4822_DAC_A, MCP4822_DAC_B};
+enum MCP4822_DAC_CHAN
+{
+    MCP4822_DAC_A,
+    MCP4822_DAC_B
+};
 
-class MCP4822 {
+class MCP4822
+{
 private:
     int cs;
     int ldac;
     int gain[MCP4822_NUMCHAN];
     int getCmdWrd(int dac, int value);
+
 public:
     MCP4822();
     MCP4822(int csPin, int ldacPin);
@@ -80,14 +86,15 @@ public:
 // Author: Will Dickson, IO Rodeo Inc.
 // ----------------------------------------------------------------------------
 
-#define ACTIVE      0b0001000000000000
-#define SHUTDOWN    0b0000000000000000
-#define GAIN_2X     0b0000000000000000
-#define GAIN_1X     0b0010000000000000
+#define ACTIVE 0b0001000000000000
+#define SHUTDOWN 0b0000000000000000
+#define GAIN_2X 0b0000000000000000
+#define GAIN_1X 0b0010000000000000
 #define DAC_A_WRITE 0b0000000000000000
 #define DAC_B_WRITE 0b1000000000000000
 
-MCP4822::MCP4822() {
+MCP4822::MCP4822()
+{
 }
 
 // ----------------------------------------------------------------------------
@@ -95,14 +102,15 @@ MCP4822::MCP4822() {
 //
 // Constructor
 // ----------------------------------------------------------------------------
-MCP4822::MCP4822(int csPin, int ldacPin) {
+MCP4822::MCP4822(int csPin, int ldacPin)
+{
     // Configure chip select and latch pins
     cs = csPin;
     ldac = ldacPin;
-    pinMode(cs,OUTPUT);
-    pinMode(ldac,OUTPUT);
-    digitalWrite(cs,HIGH);
-    digitalWrite(ldac,HIGH);
+    pinMode(cs, OUTPUT);
+    pinMode(ldac, OUTPUT);
+    digitalWrite(cs, HIGH);
+    digitalWrite(ldac, HIGH);
     // Set to default configuration
     setGain2X_AB();
 }
@@ -112,17 +120,19 @@ MCP4822::MCP4822(int csPin, int ldacPin) {
 //
 // Gets command work for writing value to given channel
 // ---------------------------------------------------------------------------
-int MCP4822::getCmdWrd(int dac, int value) {
+int MCP4822::getCmdWrd(int dac, int value)
+{
     int cmdWrd = 0;
-    switch (dac) {
-        case MCP4822_DAC_A:
-            cmdWrd = DAC_A_WRITE;
-            break;
-        case MCP4822_DAC_B:
-            cmdWrd = DAC_B_WRITE;
-            break;
-        default:
-            return 0;
+    switch (dac)
+    {
+    case MCP4822_DAC_A:
+        cmdWrd = DAC_A_WRITE;
+        break;
+    case MCP4822_DAC_B:
+        cmdWrd = DAC_B_WRITE;
+        break;
+    default:
+        return 0;
     }
     cmdWrd |= gain[dac];
     cmdWrd |= ACTIVE;
@@ -135,24 +145,25 @@ int MCP4822::getCmdWrd(int dac, int value) {
 //
 // Set the output value of the given dac channel
 // ----------------------------------------------------------------------------
-void MCP4822::setValue(int dac, int value) {
+void MCP4822::setValue(int dac, int value)
+{
     int cmdWrd;
     uint8_t byte0;
     uint8_t byte1;
 
     // Enable SPI communications
-    digitalWrite(cs,LOW);
+    digitalWrite(cs, LOW);
     // Send command word
-    cmdWrd = getCmdWrd(dac,value);
+    cmdWrd = getCmdWrd(dac, value);
     byte0 = cmdWrd >> 8;
     byte1 = cmdWrd & 0b0000000011111111;
     SPI.transfer(byte0);
     SPI.transfer(byte1);
     // Disable SPI communications
-    digitalWrite(cs,HIGH);
+    digitalWrite(cs, HIGH);
     // Latch value
-    digitalWrite(ldac,LOW);
-    digitalWrite(ldac,HIGH);
+    digitalWrite(ldac, LOW);
+    digitalWrite(ldac, HIGH);
 }
 
 // ---------------------------------------------------------------------------
@@ -160,7 +171,8 @@ void MCP4822::setValue(int dac, int value) {
 //
 // Set the output value of dac A to the given value
 // ---------------------------------------------------------------------------
-void MCP4822::setValue_A(int value) {
+void MCP4822::setValue_A(int value)
+{
     setValue(MCP4822_DAC_A, value);
 }
 
@@ -169,8 +181,9 @@ void MCP4822::setValue_A(int value) {
 //
 // Set the output value of dac B to the given value
 // ----------------------------------------------------------------------------
-void MCP4822::setValue_B(int value) {
-    setValue(MCP4822_DAC_B,value);
+void MCP4822::setValue_B(int value)
+{
+    setValue(MCP4822_DAC_B, value);
 }
 
 // ---------------------------------------------------------------------------
@@ -179,33 +192,33 @@ void MCP4822::setValue_B(int value) {
 // Set the output value of dac A and B to the given values. Latch them in at
 // the same time.
 // ---------------------------------------------------------------------------
-void MCP4822::setValue_AB(int value_A, int value_B) {
+void MCP4822::setValue_AB(int value_A, int value_B)
+{
     int cmdWrd;
     uint8_t byte0;
     uint8_t byte1;
     // Enable SPI communications and send command word
-    digitalWrite(cs,LOW);
+    digitalWrite(cs, LOW);
     // Send command word for DAC A
-    cmdWrd = getCmdWrd(MCP4822_DAC_A,value_A);
+    cmdWrd = getCmdWrd(MCP4822_DAC_A, value_A);
     byte0 = cmdWrd >> 8;
     byte1 = cmdWrd & 0b0000000011111111;
     SPI.transfer(byte0);
     SPI.transfer(byte1);
     // Toggle CS
-    digitalWrite(cs,HIGH);
-    digitalWrite(cs,LOW);
+    digitalWrite(cs, HIGH);
+    digitalWrite(cs, LOW);
     // Send command word for DAC A
-    cmdWrd = getCmdWrd(MCP4822_DAC_B,value_B);
+    cmdWrd = getCmdWrd(MCP4822_DAC_B, value_B);
     byte0 = cmdWrd >> 8;
     byte1 = cmdWrd & 0b0000000011111111;
     SPI.transfer(byte0);
     SPI.transfer(byte1);
     // Disable SPI communications
-    digitalWrite(cs,HIGH);
+    digitalWrite(cs, HIGH);
     // Latch value
-    digitalWrite(ldac,LOW);
-    digitalWrite(ldac,HIGH);
-
+    digitalWrite(ldac, LOW);
+    digitalWrite(ldac, HIGH);
 }
 
 // ---------------------------------------------------------------------------
@@ -213,8 +226,10 @@ void MCP4822::setValue_AB(int value_A, int value_B) {
 //
 // Set the gain of the given channel to 2X
 // ---------------------------------------------------------------------------
-void MCP4822::setGain2X(int dac) {
-    if ((dac == MCP4822_DAC_A) || (dac == MCP4822_DAC_B)) {
+void MCP4822::setGain2X(int dac)
+{
+    if ((dac == MCP4822_DAC_A) || (dac == MCP4822_DAC_B))
+    {
         gain[dac] = GAIN_2X;
     }
 }
@@ -224,7 +239,8 @@ void MCP4822::setGain2X(int dac) {
 //
 // Set the gain of dac A to 2X
 // ----------------------------------------------------------------------------
-void MCP4822::setGain2X_A() {
+void MCP4822::setGain2X_A()
+{
     setGain2X(MCP4822_DAC_A);
 }
 
@@ -233,7 +249,8 @@ void MCP4822::setGain2X_A() {
 //
 // Set the gain of dac B to 2X
 // ----------------------------------------------------------------------------
-void MCP4822::setGain2X_B() {
+void MCP4822::setGain2X_B()
+{
     setGain2X(MCP4822_DAC_B);
 }
 
@@ -242,7 +259,8 @@ void MCP4822::setGain2X_B() {
 //
 // Set the gain of dac A and B to 2X
 // ----------------------------------------------------------------------------
-void MCP4822::setGain2X_AB() {
+void MCP4822::setGain2X_AB()
+{
     setGain2X_A();
     setGain2X_B();
 }
@@ -252,8 +270,10 @@ void MCP4822::setGain2X_AB() {
 //
 // Set the gain of the given channel to 1X
 // ----------------------------------------------------------------------------
-void MCP4822::setGain1X(int dac) {
-    if ((dac == MCP4822_DAC_A) || (dac == MCP4822_DAC_B)) {
+void MCP4822::setGain1X(int dac)
+{
+    if ((dac == MCP4822_DAC_A) || (dac == MCP4822_DAC_B))
+    {
         gain[dac] = GAIN_1X;
     }
 }
@@ -263,7 +283,8 @@ void MCP4822::setGain1X(int dac) {
 //
 // Set the gain of dac A to 1X
 // ----------------------------------------------------------------------------
-void MCP4822::setGain1X_A() {
+void MCP4822::setGain1X_A()
+{
     setGain1X(MCP4822_DAC_A);
 }
 
@@ -272,7 +293,8 @@ void MCP4822::setGain1X_A() {
 //
 // Set the gain of dac B to 1X
 // ----------------------------------------------------------------------------
-void MCP4822::setGain1X_B() {
+void MCP4822::setGain1X_B()
+{
     setGain1X(MCP4822_DAC_B);
 }
 
@@ -281,7 +303,8 @@ void MCP4822::setGain1X_B() {
 //
 // Set the gain of dac A and B to 1X
 // ----------------------------------------------------------------------------
-void MCP4822::setGain1X_AB() {
+void MCP4822::setGain1X_AB()
+{
     setGain1X_A();
     setGain1X_B();
 }
@@ -291,32 +314,34 @@ void MCP4822::setGain1X_AB() {
 //
 // Turn off the given dac
 // ----------------------------------------------------------------------------
-void MCP4822::off(int dac) {
+void MCP4822::off(int dac)
+{
     int cmdWrd = 0;
     uint8_t byte0;
     uint8_t byte1;
 
     // Create shutdown cmd word.
-    switch (dac) {
-        case MCP4822_DAC_A:
-            cmdWrd = DAC_A_WRITE;
-            break;
-        case MCP4822_DAC_B:
-            cmdWrd = DAC_B_WRITE;
-            break;
-        default:
-            return;
+    switch (dac)
+    {
+    case MCP4822_DAC_A:
+        cmdWrd = DAC_A_WRITE;
+        break;
+    case MCP4822_DAC_B:
+        cmdWrd = DAC_B_WRITE;
+        break;
+    default:
+        return;
     }
     cmdWrd |= SHUTDOWN;
     // Enable SPI communications
-    digitalWrite(cs,LOW);
+    digitalWrite(cs, LOW);
     // Send command word
     byte0 = cmdWrd >> 8;
     byte1 = cmdWrd & 0b0000000011111111;
     SPI.transfer(byte0);
     SPI.transfer(byte1);
     // Disable SPI communications
-    digitalWrite(cs,HIGH);
+    digitalWrite(cs, HIGH);
 }
 
 // ----------------------------------------------------------------------------
@@ -324,7 +349,8 @@ void MCP4822::off(int dac) {
 //
 // Turn off dac A
 // ----------------------------------------------------------------------------
-void MCP4822::off_A() {
+void MCP4822::off_A()
+{
     off(MCP4822_DAC_A);
 }
 
@@ -333,7 +359,8 @@ void MCP4822::off_A() {
 //
 // Turn off dac B
 // ----------------------------------------------------------------------------
-void MCP4822::off_B() {
+void MCP4822::off_B()
+{
     off(MCP4822_DAC_B);
 }
 
@@ -342,7 +369,8 @@ void MCP4822::off_B() {
 //
 // Turn off dac A and B
 // ----------------------------------------------------------------------------
-void MCP4822::off_AB() {
+void MCP4822::off_AB()
+{
     off_A();
     off_B();
 }
@@ -353,7 +381,8 @@ void MCP4822::off_AB() {
 
 typedef uint32_t pulsenumber_dtype; /* 2**32 @100Hz = 497 days */
 
-struct timed_sample {
+struct timed_sample
+{
     uint8_t value; /* value of arbitrary data */
     pulsenumber_dtype pulsenumber;
     uint16_t ticks;
@@ -368,14 +397,14 @@ const unsigned short AOUT_LDAC = 7;
 #endif
 
 // Global variables ------------------------------------------------------------
-volatile pulsenumber_dtype pulsenumber=0;
-    // ---- set TCCR1B ----------
-    // high bits = 0,0,0
-    // WGM13, WGM12 = 1,1
-    // CS1 = 0,1,0 (starts timer1 CS=8) (clock select)
+volatile pulsenumber_dtype pulsenumber = 0;
+// ---- set TCCR1B ----------
+// high bits = 0,0,0
+// WGM13, WGM12 = 1,1
+// CS1 = 0,1,0 (starts timer1 CS=8) (clock select)
 uint8_t tccr1b_when_running = 0x18; //stopped. started in setup()
 #ifdef WITH_AOUT
-MCP4822 analogOut = MCP4822(AOUT_CS,AOUT_LDAC);
+MCP4822 analogOut = MCP4822(AOUT_CS, AOUT_LDAC);
 #endif
 
 UDEV udev(Serial);
@@ -387,7 +416,8 @@ ISR(TIMER1_COMPA_vect)
 }
 
 // Setup timer1 ----------------------------------------------------------------
-void setup_timer1(uint8_t tccr1b, uint16_t icr1) {
+void setup_timer1(uint8_t tccr1b, uint16_t icr1)
+{
     tccr1b_when_running = tccr1b;
 
     cli();
@@ -412,9 +442,10 @@ void setup_timer1(uint8_t tccr1b, uint16_t icr1) {
 }
 
 // Standard arduino setup function ---------------------------------------------
-void setup() {
+void setup()
+{
     udev.begin();
-    udev.serial_handshake();   //blocks for 5 seconds by default
+    udev.serial_handshake(); //blocks for 5 seconds by default
 
     SPI.setDataMode(SPI_MODE0);
     SPI.setBitOrder(MSBFIRST);
@@ -438,21 +469,25 @@ void setup() {
 }
 
 // Send data with our simple protocol to the host computer ---------------------
-static char Serial_read_blocking() {
-    while (Serial.available() == 0) {
+static char Serial_read_blocking()
+{
+    while (Serial.available() == 0)
+    {
         delay(10);
     }
     return Serial.read();
 }
 
-static inline void send_data_string(String* s, const char header) {
+static inline void send_data_string(String *s, const char header)
+{
     uint8_t N = s->length();
 
     Serial.write(header);
     Serial.write(N); // payload size
-    uint8_t chksum=0;
+    uint8_t chksum = 0;
     char next_byte;
-    for (uint8_t i=0; i< N; i++) {
+    for (uint8_t i = 0; i < N; i++)
+    {
         next_byte = s->charAt(i);
         chksum += next_byte;
         Serial.write(next_byte);
@@ -460,7 +495,8 @@ static inline void send_data_string(String* s, const char header) {
     Serial.write(chksum);
 }
 
-static inline void send_data(const struct timed_sample samp, const char header) {
+static inline void send_data(const struct timed_sample samp, const char header)
+{
 
     /* This commented-out version does the same as below but with more
        overhead. Nevertheless, it can be used to verify the
@@ -477,12 +513,13 @@ static inline void send_data(const struct timed_sample samp, const char header) 
     send_data_string(&out_buf, header);
     */
 
-    const char * buf;
+    const char *buf;
     Serial.write(header);
     Serial.write((char)sizeof(struct timed_sample)); // payload size
-    buf = (const char*)&(samp);
-    uint8_t chksum=0;
-    for (uint8_t i=0; i< sizeof(struct timed_sample); i++) {
+    buf = (const char *)&(samp);
+    uint8_t chksum = 0;
+    for (uint8_t i = 0; i < sizeof(struct timed_sample); i++)
+    {
         chksum += buf[i];
         Serial.write(buf[i]);
     }
@@ -490,92 +527,110 @@ static inline void send_data(const struct timed_sample samp, const char header) 
 }
 
 // Standard Arduino loop function. This gets repeatedly called at high rate ----
-void loop() {
-    if (Serial.available() >= 2) {
+void loop()
+{
+    if (Serial.available() >= 2)
+    {
         static char cmd;
         static char value;
 
         cmd = Serial.read();
         value = Serial.read();
 
-        if (cmd=='P') {
+        if (cmd == 'P')
+        {
             // timestamp query
             static struct timed_sample timestamp_request;
 
             timestamp_request.value = value;
 
-            uint8_t SaveSREG_ = SREG;   // save interrupt flag
-            cli(); // disable interrupts
+            uint8_t SaveSREG_ = SREG; // save interrupt flag
+            cli();                    // disable interrupts
 
-                timestamp_request.pulsenumber = pulsenumber;
-                timestamp_request.ticks = TCNT1;
+            timestamp_request.pulsenumber = pulsenumber;
+            timestamp_request.ticks = TCNT1;
 
             SREG = SaveSREG_; // restore interrupt flags
-            send_data(timestamp_request,'P');
-        } else if (cmd=='V') {
+            send_data(timestamp_request, 'P');
+        }
+        else if (cmd == 'V')
+        {
             // version request
             static struct timed_sample version_request;
 
             version_request.value = 14;
 
-            uint8_t SaveSREG_ = SREG;   // save interrupt flag
-            cli(); // disable interrupts
+            uint8_t SaveSREG_ = SREG; // save interrupt flag
+            cli();                    // disable interrupts
 
-                version_request.pulsenumber = pulsenumber;
-                version_request.ticks = TCNT1;
+            version_request.pulsenumber = pulsenumber;
+            version_request.ticks = TCNT1;
 
             SREG = SaveSREG_; // restore interrupt flags
-            send_data(version_request,'V');
+            send_data(version_request, 'V');
 
             digitalWrite(LEDPin, LOW);
-
-        } else if (cmd=='S') {
+        }
+        else if (cmd == 'S')
+        {
             // synchronization
 
-            if (value=='0') {
+            if (value == '0')
+            {
                 // stop clock, reset pulsenumber
                 TCCR1B = 0x18;
                 TCNT1 = 0;
                 pulsenumber = 0;
                 digitalWrite(LEDPin, HIGH);
-            } else if (value=='1') {
+            }
+            else if (value == '1')
+            {
                 // start clock
                 TCCR1B = tccr1b_when_running;
                 digitalWrite(LEDPin, LOW);
-            } else if (value=='2') {
+            }
+            else if (value == '2')
+            {
                 // stop clock
                 TCCR1B = 0x18;
                 digitalWrite(LEDPin, HIGH);
             }
-
-        } else if (cmd=='T') {
+        }
+        else if (cmd == 'T')
+        {
             // TOP value
             uint8_t value0, value1;
             char prescaler_key;
             uint16_t new_icr1;
 
-            if (value=='=') {
+            if (value == '=')
+            {
                 value0 = Serial_read_blocking();
                 value1 = Serial_read_blocking();
                 prescaler_key = Serial_read_blocking();
                 new_icr1 = ((uint16_t)value1 << 8) + value0;
                 ICR1 = new_icr1;
-                if (prescaler_key=='1') {
+                if (prescaler_key == '1')
+                {
                     // prescaler = 8
                     tccr1b_when_running = 0x1A;
-                } else if (prescaler_key=='2') {
+                }
+                else if (prescaler_key == '2')
+                {
                     // prescaler = 64
                     tccr1b_when_running = 0x1B;
                 }
             }
-
-        } else if (cmd=='O') {
+        }
+        else if (cmd == 'O')
+        {
             // AOUT values
-            uint8_t aout0_0, aout0_1,   aout1_0, aout1_1;
+            uint8_t aout0_0, aout0_1, aout1_0, aout1_1;
             uint8_t aout_sequence;
             int aout0, aout1;
 
-            if (value=='=') {
+            if (value == '=')
+            {
                 aout0_0 = Serial_read_blocking();
                 aout0_1 = Serial_read_blocking();
                 aout0 = ((int)aout0_1 << 8) + aout0_0;
@@ -589,26 +644,23 @@ void loop() {
 #endif
                 aout_sequence = Serial_read_blocking();
 
+                static struct timed_sample aout_confirm;
 
-            static struct timed_sample aout_confirm;
+                aout_confirm.value = aout_sequence;
 
-            aout_confirm.value = aout_sequence;
-
-            uint8_t SaveSREG_ = SREG;   // save interrupt flag
-            cli(); // disable interrupts
+                uint8_t SaveSREG_ = SREG; // save interrupt flag
+                cli();                    // disable interrupts
 
                 aout_confirm.pulsenumber = pulsenumber;
                 aout_confirm.ticks = TCNT1;
 
-            SREG = SaveSREG_; // restore interrupt flags
-            send_data(aout_confirm,'O');
-
+                SREG = SaveSREG_; // restore interrupt flags
+                send_data(aout_confirm, 'O');
             }
-
-        } else if (cmd=='N') {
+        }
+        else if (cmd == 'N')
+        {
             udev.process(cmd, value);
         }
-
     }
-
 }
