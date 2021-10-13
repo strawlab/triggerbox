19d3f513360cdcfe8ff8f3541a273ec6c1d36bdc
Andrew Straw <strawman@astraw.com>
2021-10-13 10:10:03 +0200

    extract fit_time_model() function and test it

--- braid-triggerbox-rs/src/lib.rs
+++ braid-triggerbox-rs/src/lib.rs
@@ -411,27 +411,9 @@ impl SerialThread {
                 ));
 
                 if self.past_data.len() >= 5 {
-                    use na::{OMatrix, OVector, U2};
-
-                    // fit time model
-                    let mut a: Vec<f64> = Vec::with_capacity(self.past_data.len() * 2);
-                    let mut b: Vec<f64> = Vec::with_capacity(self.past_data.len());
-
-                    for row in self.past_data.iter() {
-                        a.push(row.0);
-                        a.push(1.0);
-                        b.push(row.1);
-                    }
-                    let a = OMatrix::<f64, na::Dynamic, U2>::from_row_slice(&a);
-                    let b = OVector::<f64, na::Dynamic>::from_row_slice(&b);
-
-                    let epsilon = 1e-10;
-                    let results = lstsq::lstsq(&a, &b, epsilon)
+                    let (gain, offset, residuals) = fit_time_model(&self.past_data)
                         .map_err(|e| anyhow::anyhow!("lstsq err: {}", e))?;
 
-                    let gain = results.solution[0];
-                    let offset = results.solution[1];
-                    let residuals = results.residuals;
                     let n_measurements = self.past_data.len() as u64;
                     let per_point_residual = residuals / n_measurements as f64;
                     // TODO only accept this if residuals less than some amount?
@@ -508,6 +490,45 @@ impl SerialThread {
         }
         Ok(buf)
     }
+}
+
+fn fit_time_model(past_data: &[(f64, f64)]) -> Result<(f64, f64, f64), &'static str> {
+    use na::{OMatrix, OVector, U2};
+
+    let mut a: Vec<f64> = Vec::with_capacity(past_data.len() * 2);
+    let mut b: Vec<f64> = Vec::with_capacity(past_data.len());
+
+    for row in past_data.iter() {
+        a.push(row.0);
+        a.push(1.0);
+        b.push(row.1);
+    }
+    let a = OMatrix::<f64, na::Dynamic, U2>::from_row_slice(&a);
+    let b = OVector::<f64, na::Dynamic>::from_row_slice(&b);
+
+    let epsilon = 1e-10;
+    let results = lstsq::lstsq(&a, &b, epsilon)?;
+
+    let gain = results.solution[0];
+    let offset = results.solution[1];
+    let residuals = results.residuals;
+
+    Ok((gain, offset, residuals))
+}
+
+#[test]
+fn test_fit_time_model() {
+    let epsilon = 1e-12;
+
+    let data = vec![(0.0, 0.0), (1.0, 1.0), (2.0, 2.0), (3.0, 3.0)];
+    let (gain, offset, _residuals) = fit_time_model(&data).unwrap();
+    assert!((gain - 1.0).abs() < epsilon);
+    assert!((offset - 0.0).abs() < epsilon);
+
+    let data = vec![(0.0, 12.0), (1.0, 22.0), (2.0, 32.0), (3.0, 42.0)];
+    let (gain, offset, _residuals) = fit_time_model(&data).unwrap();
+    assert!((gain - 10.0).abs() < epsilon);
+    assert!((offset - 12.0).abs() < epsilon);
 }
 
 pub fn launch_background_thread(
